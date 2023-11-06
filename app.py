ee2a41c98049a410f7928c96263a96c4d62fc0f9
Sumit Kumar <sumit821044@gmail.com>
2023-11-06 13:56:28 +0530

    Update app.py
    complete working model

--- app.py
+++ app.py
@@ -1,38 +1,28 @@
-from sklearn.feature_extraction.text import CountVectorizer
-
-
 from flask import Flask, render_template, request
 import pickle
 
-tokenizer = pickle.load(open("templates/models/cv.pkl", "rb"))
-model = pickle.load(open("templates/models/clf.pkl", "rb"))
-
+cv = pickle.load(open("templates/models/new_cv.pkl", "rb"))
+model = pickle.load(open("templates/models/new_clf.pkl", "rb"))
+#create an object of flask name
 app = Flask(__name__)
 #says about home page
 @app.route("/")
 def home():
     return render_template("index.html")
-    #home we don't need these things
-    # text = ""
-    # if request.method == 'POST':
-    #     text = request.form.get('email-content')
-    # return render_template('index.html' ,text = text)
 
 @app.route("/predict", methods = ['POST']) # only get when click on button
 def predict():
-    
-    message_text = request.form.get('email-content')
-    cv = CountVectorizer()
-    tokenized_message = cv.transform([message_text]).toarray()
 
-    # tokenized_message = tokenizer.transform([message_text])
+    if request.method == 'POST':
+        # get message from form
 
+        message_text = request.form.get('content')
+        data = cv.transform([message_text]).toarray()
+        #pridicting by using our model
+        prediction = model.predict(data)[0]
 
-    prediction = model.predict(tokenized_message)
-    prediction = int(prediction)
-    prediction = 1 if prediction == 1 else 0
-    return render_template("index.html", prediction = prediction, message_text = message_text)
+        print(prediction)
+        return render_template("index.html", prediction = prediction, message_text = message_text)
 
 if __name__ == "__main__":
     app.run( host = "0.0.0.0" , port = "8080" , debug = True)   #debug mode
-
